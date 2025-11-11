53b5e26d628392b93af4fcbe1ddde96a00eb0592
anantariskys <gamersindo555@gmail.com>
2025-11-11 21:43:48 +0700

    feat(api): add dashboard and classification history routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\RiwayatKlasifikasiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,5 +15,15 @@ Route::get('/user', function (Request $request) {
 Route::post('/auth/social-login', [AuthController::class, 'socialLogin']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
     Route::get('dosen/{bidangPenelitian}', [DosenController::class, 'getDosenByBidang']);
+
+    Route::prefix('riwayat-klasifikasi')->group(function () {
+        Route::get('/', [RiwayatKlasifikasiController::class, 'index']);
+        Route::get('/user/{userId}', [RiwayatKlasifikasiController::class, 'findByUser']);
+        Route::post('/', [RiwayatKlasifikasiController::class, 'store']);
+        Route::get('/{id}', [RiwayatKlasifikasiController::class, 'show']);
+        Route::delete('/{id}', [RiwayatKlasifikasiController::class, 'destroy']);
+    });
 });
